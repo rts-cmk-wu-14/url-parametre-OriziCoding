b6fc07dcc22973e215937bff8c59aa9aae6faa97
Mathias <orizigaming@gmail.com>
2025-09-01 14:03:11 +0200

    frontpage almost done

--- java.js
+++ java.js
@@ -1,9 +1,24 @@
 fetch('data/destinations.json')
     .then(response => response.json())
     .then(data => handledestinations(data))
-    .catch(error => console.error(error));
+    
 function handledestinations(data){
     console.log (data)
-    
+    let first
 
-}
+    data.destinations.forEach((dest, i) => {
+        first = `
+        <div class="container">
+            <img class="thumbnail" src="/img/${data.destinations[i].image}">
+            <div class="bottom">
+                <img class="svg" src="/img/heart.svg">
+                <a href="">More</a>
+            </div>
+        </div>
+        
+        `
+        document.getElementById("grid").insertAdjacentHTML("beforeend", first)
+
+})}
+
+
